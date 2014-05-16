ebe313505dd7ddc25bfe8662f2abfc420d24f175
张宇翔 <zz593141477@gmail.com>
2014-05-16 13:09:28 +0800

    fix bug: wrong output channel

--- Project/src/extruder.c
+++ Project/src/extruder.c
@@ -47,7 +47,7 @@ void Extruder_Start_Heating(uint16_t _target)
 {
 	Fan_Enable(true);
 	PID_Init(&pid, EXTRUDER_PID_KP, EXTRUDER_PID_KI, EXTRUDER_PID_KD, EXTRUDER_PID_INIT_SUM);
-	PWM_Channel(HBP_Ch, 90, false);
+	PWM_Channel(Ex1Heat_Ch, 90, false);
 	currentOutput = 0;
 	bHeating = true;
 	targetTemp = _target;
@@ -56,7 +56,7 @@ void Extruder_Start_Heating(uint16_t _target)
 void Extruder_Stop_Heating()
 {
 	Fan_Enable(false);
-	PWM_Channel(HBP_Ch, 10, false);
+	PWM_Channel(Ex1Heat_Ch, 10, false);
 	currentOutput = 0;
 	bHeating = false;
 }
@@ -77,7 +77,7 @@ void Extruder_SetOutput(int output)
 	else if(output < 0)
 		output = 0;
 
-	PWM_Channel(HBP_Ch, output, true);
+	PWM_Channel(Ex1Heat_Ch, output, true);
 	currentOutput = output;
 }
 
