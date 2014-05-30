5108942207e345bdca6ba64eea0e74d040b6caa3
张宇翔 <zz593141477@gmail.com>
2014-05-30 20:36:03 +0800

    update params

--- Project/include/configure.h
+++ Project/include/configure.h
@@ -24,26 +24,26 @@
 //从G代码坐标到实际坐标的偏移(um)
 #define X_OFFSET (X_MAX_LIMIT/2-15000)
 #define Y_OFFSET (Y_MAX_LIMIT/2)
-#define Z_OFFSET 1420
+#define Z_OFFSET 1720
 
 //X轴步进电机旋转一周对应的直线位移(um)
 #define X_DISTANCE_PER_CYCLE 4000
 //X轴步进电机旋转一周需要的脉冲数量(考虑细分)
-#define X_PULSES_PER_CYCLE   (4*200)
+#define X_PULSES_PER_CYCLE   (16*200)
 //X轴运行方向调整(取值+/-1)
 #define X_DIRECTION_ADJ      1 
 
 //Y轴步进电机旋转一周对应的直线位移(um)
 #define Y_DISTANCE_PER_CYCLE 4000
 //Y轴步进电机旋转一周需要的脉冲数量(考虑细分)
-#define Y_PULSES_PER_CYCLE (4*200)
+#define Y_PULSES_PER_CYCLE (16*200)
 //Y轴运行方向调整(取值+/-1)
 #define Y_DIRECTION_ADJ      -1 
 
 //Z轴步进电机旋转一周对应的直线位移(um)
 #define Z_DISTANCE_PER_CYCLE 4000
 //Z轴步进电机旋转一周需要的脉冲数量(考虑细分)
-#define Z_PULSES_PER_CYCLE (4*200)
+#define Z_PULSES_PER_CYCLE (16*200)
 //Z轴运行方向调整(取值+/-1)
 #define Z_DIRECTION_ADJ      1
 
@@ -65,12 +65,12 @@
 //挤出器电机方向修正(取值+/-1)
 #define EXTRUDER_MOTOR_DIR -1
 //挤出量系数调整(越大量越少)
-#define EXTRUDER_VOLUME_ADJ 40
+#define EXTRUDER_VOLUME_ADJ 10
 
 //挤出器输出更新频率限制(ms)
 #define EXTRUDER_UPDATE_PERIOD 500
 //挤出器默认温度
-#define EXTRUDER_DEFAULT_TEMP 245
+#define EXTRUDER_DEFAULT_TEMP 200
 //挤出器温度PID控制参数
 #define EXTRUDER_PID_KP 800
 #define EXTRUDER_PID_KI 25
@@ -80,7 +80,7 @@
 //加热板输出更新频率限制(ms)
 #define HEATBED_UPDATE_PERIOD 500
 //加热板默认温度
-#define HEATBED_DEFAULT_TEMP 100
+#define HEATBED_DEFAULT_TEMP 96
 //加热板传感器值与温度换算
 #define HEATBED_ADC_TO_TEMP(x) (123.678-0.0893583*(x)+0.0000195876*(x)*(x))
 //加热板PID参数
@@ -93,7 +93,7 @@
 // #define DRIVER_BOARD_POLARITY
 
 //步进电机脉冲频率(单位Hz)
-#define MOTOR_STEP_FREQ 2400
+#define MOTOR_STEP_FREQ 8000
 
 //SD卡初始化重试次数限制
 #define SD_INIT_RETRY_TIMES 3
