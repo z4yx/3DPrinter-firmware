52722c4a4e58d08d72611987e101bd462ae90604
张宇翔 <zz593141477@gmail.com>
2014-08-19 08:38:51 +0800

    better implementation of timer calculation

--- Project/src/common.c
+++ Project/src/common.c
@@ -66,14 +66,13 @@ void RCC_USARTClockCmd(USART_TypeDef* USARTx, FunctionalState state)
 //根据频率(Hz)计算TIM的参数
 void Timer_16bit_Calc(int freq, uint16_t *period, uint16_t *prescaler)
 {
-	*period = SystemCoreClock / freq;
-
-	if(*period > 60000) {
-		*period = SystemCoreClock / 5000 / freq - 1;
-		*prescaler = 5000 - 1;
+	uint32_t tmp = SystemCoreClock / freq;
+	*prescaler = 1;
+	while(tmp > 0xffff){
+		*prescaler <<= 1;
+		tmp >>= 1;
 	}
-	else {
-		(*period)--;
-		*prescaler = 0;
+	(*prescaler) --;
+	*period = tmp-1;
 	}
 }
