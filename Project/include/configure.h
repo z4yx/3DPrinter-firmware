d3c774de2124c0712abc4ef274352a70f24ed861
张宇翔 <zz593141477@gmail.com>
2014-08-29 21:11:49 +0800

    update axis parameter

--- Project/include/configure.h
+++ Project/include/configure.h
@@ -42,21 +42,21 @@
 #endif
 
 //X轴步进电机旋转一周对应的直线位移(um)
-#define X_DISTANCE_PER_CYCLE 50000
+#define X_DISTANCE_PER_CYCLE 47500
 //X轴步进电机旋转一周需要的脉冲数量(考虑细分)
 #define X_PULSES_PER_CYCLE   (16*200)
 //X轴运行方向调整(取值+/-1)
 #define X_DIRECTION_ADJ      1 
 
 //Y轴步进电机旋转一周对应的直线位移(um)
-#define Y_DISTANCE_PER_CYCLE 60000
+#define Y_DISTANCE_PER_CYCLE 66000
 //Y轴步进电机旋转一周需要的脉冲数量(考虑细分)
 #define Y_PULSES_PER_CYCLE (16*200)
 //Y轴运行方向调整(取值+/-1)
 #define Y_DIRECTION_ADJ      1 
 
 //Z轴步进电机旋转一周对应的直线位移(um)
-#define Z_DISTANCE_PER_CYCLE 4000
+#define Z_DISTANCE_PER_CYCLE 8000
 //Z轴步进电机旋转一周需要的脉冲数量(考虑细分)
 #define Z_PULSES_PER_CYCLE (16*200)
 //Z轴运行方向调整(取值+/-1)
